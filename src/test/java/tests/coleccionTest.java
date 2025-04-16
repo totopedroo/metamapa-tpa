84ebf74567cfe5afff026dd2caf2849abfc031d5
bautistaiglesias <biglesiasbasso@frba.utn.edu.ar>
2025-04-16 11:08:50 -0300

    agregfo carpetas de test, borro clase leercsv y junto en coleccion.java, ya implementada la lectura con el criterioDePertenencia

---
+++ src/test/java/tests/coleccionTest.java
@@ -0,0 +1,12 @@
+public class coleccionTest {
+package ar.edu;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+
+}
