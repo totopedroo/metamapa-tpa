deed1cfb13cfad5bd8dc4275cb6d92f97b970d6c
bautistaiglesias <164692451+bautistaiglesias@users.noreply.github.com>
2025-04-17 19:54:03 -0300

    actualizoHecho.java

--- src/main/java/domain/Hecho.java
+++ src/main/java/domain/Hecho.java
@@ -1,4 +1,39 @@
 package domain;
 
 public class Hecho {
+  package domain; // hechos branch
+
+
+import lombok.Getter;
+import java.time.LocalDate;
+import java.util.Optional;
+
+
+@Getter
+public class Hecho {
+    public String titulo;
+    public String descripcion;
+    public String categoria;
+    public Optional<ContenidoMultimedia> contenidoMultimedia;
+    public Double latitud;
+    public Double longitud;
+    public LocalDate fechaAcontecimiento;
+    public LocalDate fechaCarga;
+    public Origen origen;
+
+
+    public Hecho(String titulo, String descripcion, String categoria, ContenidoMultimedia contenidoMultimedia,
+                 Double latitud, Double longitud, LocalDate fechaAcontecimiento, LocalDate fechaCarga,
+                 Origen origen) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.contenidoMultimedia = Optional.ofNullable(contenidoMultimedia);
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.fechaAcontecimiento = fechaAcontecimiento;
+        this.fechaCarga = fechaCarga;
+        this.origen = origen;
+    }
+}
 }
