1dc07d995aeb08d1f4b209b0b1b587c2f9d0b7e7
LucasVerdaguer <lverdaguer@frba.utn.edu.ar>
2025-04-20 20:31:22 -0300

    Muevo Hecho a Admin

--- src/main/java/domain/Hecho.java
+++ src/main/java/domain/Hecho.java
@@ -1,6 +1,8 @@
 package domain;
 import lombok.Getter;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Hecho {
@@ -11,12 +13,14 @@ public class Hecho {
     public Double latitud;
     public Double longitud;
     public LocalDate fechaAcontecimiento;
-    //public LocalDate fechaCarga;
-    //public Origen origen;
-
+    public LocalDate fechaCarga;
+    public Origen origen;
+    public List<String> etiquetas = new ArrayList();
+    public List <Solicitud> solicitudes= new ArrayList();
 
     public Hecho(String titulo, String descripcion, String categoria, ContenidoMultimedia contenidoMultimedia,
-                 Double latitud, Double longitud, LocalDate fechaAcontecimiento) {
+                 Double latitud, Double longitud, LocalDate fechaAcontecimiento, LocalDate fechaCarga,
+                 Origen origen) {
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.categoria = categoria;
@@ -24,7 +28,11 @@ public class Hecho {
         this.latitud = latitud;
         this.longitud = longitud;
         this.fechaAcontecimiento = fechaAcontecimiento;
-        // this.fechaCarga = fechaCarga;
-        //this.origen = origen;
+        this.fechaCarga = fechaCarga;
+        this.origen = origen;
+    }
+
+    public void agregarSolicitud(Solicitud solicitud) {
+        solicitudes.add(solicitud);
     }
 }
