c3032fc11e527bb84b1b9fdde4851a557b9c6b9c
Bautista Iglesias <164692451+bautistaiglesias@users.noreply.github.com>
2025-04-23 13:28:16 -0300

    saco comentario de Coleccion.java

--- src/main/java/domain/Coleccion.java
+++ src/main/java/domain/Coleccion.java
@@ -94,7 +94,7 @@ public class Coleccion {
                 if (cumpleTodos) {
                     System.out.println("Fila válida: " + filaComoMapa);
 
-                    // Construir Hecho
+                    
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
                     Hecho hecho = new Hecho(
                         filaComoMapa.get("titulo"),
