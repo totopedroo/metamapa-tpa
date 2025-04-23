69e5656961c90403bf007a78a7c00b908a2a19f4
Fausto Chattas <i-fausto.chattas@devrev.ai>
2025-04-23 17:28:42 -0300

    metodo visualiarHechos (ve todos los hechos de la coleccion sin filtrar)

--- src/main/java/domain/Visualizador.java
+++ src/main/java/domain/Visualizador.java
@@ -11,9 +11,17 @@ public class Visualizador {
         this.nombre = nombre;
     }
 
+    public void visualizarHechos(Coleccion coleccion) {
+        System.out.println("TODOS los hechos disponibles en la colección '" + coleccion.getTitulo() + "' para " + nombre + ":");
+        for (Hecho hecho : coleccion.getHechos()) {
+            System.out.println("-> " + hecho.getTitulo());
+        }
+    }
+
     public void agregarFiltro(CriterioDePertenencia criterio) {
         filtrosPersonales.add(criterio);
     }
+
     public void visualizarHechosFiltrados(Coleccion coleccion) {
         System.out.println("Hechos visibles para " + nombre + " en colección '" + coleccion.getTitulo() + "':");
         for (Hecho hecho : coleccion.getHechos()) {
