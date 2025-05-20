6542f6fefa060a929924cef1f9323ab41ed2ac77
bautistaiglesias <biglesiasbasso@frba.utn.edu.ar>
2025-05-20 15:12:15 -0300

    agrego clases del diagrama

---
+++ src/main/java/ar/edu/utn/frba/domain/EtiquetadorDeHechos.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frba.domain;
+import java.util.List;
+import java.util.ArrayList;
+public class EtiquetadorDeHechos {
+    Hecho hecho;
+    List<String> etiquetas;
+}
---
+++ src/main/java/ar/edu/utn/frba/domain/Fuente.java
@@ -0,0 +1,5 @@
+package ar.edu.utn.frba.domain;
+
+public class Fuente {
+    String path;
+}
