555c69f95bb3250c9fc79d0d51e88aa3e7b02046
jkarabin <jkarabin@frba.utn.edu.ar>
2025-05-25 12:29:06 -0300

    creo ApiDesastresResponse para deserializar mejor

---
+++ src/main/java/ar/edu/utn/frba/domain/ApiDesastresResponse.java
@@ -0,0 +1,25 @@
+package ar.edu.utn.frba.domain;
+
+import ar.edu.utn.frba.Dtos.DesastreDto;
+import java.util.List;
+
+public class ApiDesastresResponse {
+  private int current_page;
+  private List<DesastreDto> data;
+
+  public List<DesastreDto> getData() {
+    return data;
+  }
+
+  public void setData(List<DesastreDto> data) {
+    this.data = data;
+  }
+
+  public int getCurrent_page() {
+    return current_page;
+  }
+
+  public void setCurrent_page(int current_page) {
+    this.current_page = current_page;
+  }
+}
