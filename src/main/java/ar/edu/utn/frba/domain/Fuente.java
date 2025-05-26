9dc54996233db8037b503161cdf3697733244b78
Tomas Pedro <tpedro@frba.utn.edu.ar>
2025-05-26 18:31:44 -0300

    Actualizo esProxy() a enum por TipoDeFuente

--- src/main/java/ar/edu/utn/frba/domain/Fuente.java
+++ src/main/java/ar/edu/utn/frba/domain/Fuente.java
@@ -1,19 +1,20 @@
 package ar.edu.utn.frba.domain;
 
+import ar.edu.utn.frba.Enums.TipoFuente;
 import java.util.List;
 
 public class Fuente {
   private String path;
   private Importador importador;
-  private boolean esProxy;
+  private TipoFuente tipo;
 
-  public Fuente(String path, Importador importador, boolean esProxy) {
+  public Fuente(String path, Importador importador, TipoFuente tipo) {
     this.path = path;
     this.importador = importador;
-    this.esProxy = esProxy;
+    this.tipo = tipo;
   }
 
-  public boolean esFuenteProxy() { return esProxy;}
+  public boolean esFuenteProxy() { return tipo == TipoFuente.PROXY;}
 
   public List<Hecho> obtenerHechos() {
     return importador.importar(this);
