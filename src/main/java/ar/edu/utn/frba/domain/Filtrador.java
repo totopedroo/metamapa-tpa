efd912ffe2102854538b96017595c50bf062552c
jkarabin <jkarabin@frba.utn.edu.ar>
2025-05-27 20:22:25 -0300

    Borro clases que no se usan y codigo comentado

--- src/main/java/ar/edu/utn/frba/domain/Filtrador.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.utn.frba.domain;
-import java.util.List;
-import java.util.ArrayList;
-public class Filtrador {
-    List<CriterioDePertenencia> criterios;
-    List<Hecho> hechos;
-}
-
-
