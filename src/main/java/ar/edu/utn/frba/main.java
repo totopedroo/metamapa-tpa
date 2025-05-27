66d5a3e950f43709022dc2913202a56fd67ed515
jkarabin <jkarabin@frba.utn.edu.ar>
2025-05-27 19:28:45 -0300

    Fix formato del csv

--- src/main/java/ar/edu/utn/frba/main.java
+++ src/main/java/ar/edu/utn/frba/main.java
@@ -3,7 +3,7 @@ package ar.edu.utn.frba; // develop branch
 import ar.edu.utn.frba.domain.ImportadorCSV;
 
 public class main {
-    ImportadorCSV importadorCSV = new ImportadorCSV()
+    ImportadorCSV importadorCSV = new ImportadorCSV();
 
 
 
